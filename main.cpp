4eb7a779a9157e0a8b52ad9ce07e1ab98c0f9cd8
Filippo Mutta <filippo.mutta@gmail.com>
2024-02-11 10:37:50 +0100

    Using new method to display memory areas.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <mkmi_syscall.h>
+#include <mkmi_capability.h>
 
 void PutStr(const char *str) {
 	Syscall(0, (size_t)str, 's', 0, 0, 0, 0);
@@ -14,23 +15,27 @@ extern "C" int Main(void *parent) {
 	PutStr("Hello, from userland!\r\n");
 
 	uintptr_t cnodePtr = 0;
-	uintptr_t memoryRegion = 0;
 	size_t cnodeSlot = 4;
-	size_t capSize = 0;
-	Syscall(2, cnodePtr, cnodeSlot, (size_t)&cnodePtr, (size_t)&capSize, 0, 0);
+
+	MKMI_Capability cnodeCapability;
+	MKMI_Capability memoryCapability;
+
+	Syscall(2, cnodePtr, cnodeSlot, (size_t)&cnodeCapability, 0, 0, 0);
 	cnodeSlot = 1;
 
 	PutStr("Memory regions:\r\n");
-	while (capSize != 0) {
-		Syscall(2, cnodePtr, cnodeSlot, (size_t)&memoryRegion, (size_t)&capSize, 0, 0);
+	do {
+		Syscall(2, cnodeCapability.Object, cnodeSlot, (size_t)&memoryCapability, 0, 0, 0);
 		
-		PutHex(memoryRegion);
+		PutHex(memoryCapability.Type);
 		PutStr("\r\n");
-		PutHex(capSize);
+		PutHex(memoryCapability.Object);
+		PutStr("\r\n");
+		PutHex(memoryCapability.Size);
 		PutStr("\r\n");
 
 		++cnodeSlot;
-	}
+	} while(memoryCapability.Type != MKMI_ObjectType::NULL_CAPABILITY);
 
 	(void)parent;
 	return 0;
