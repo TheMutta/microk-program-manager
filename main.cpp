54d017cf45c99254e4c592c8920cf029419b3acd
Filippo Mutta <filippo.mutta@gmail.com>
2024-02-13 12:23:33 +0100

    Mapping pages.

--- main.cpp
+++ main.cpp
@@ -4,29 +4,31 @@
 #include <mkmi_capability.h>
 
 void PutStr(const char *str) {
-	Syscall(0, (size_t)str, 's', 0, 0, 0, 0);
+	Syscall(SYSCALL_VECTOR_DEBUG, (usize)str, 's', 0, 0, 0, 0);
 }
 
-void PutHex(size_t hex) {
-	Syscall(0, hex, 'x', 0, 0, 0, 0);
+void PutHex(usize hex) {
+	Syscall(SYSCALL_VECTOR_DEBUG, hex, 'x', 0, 0, 0, 0);
 }
 
+void PutDec(long dec) {
+	Syscall(SYSCALL_VECTOR_DEBUG, dec, 'd', 0, 0, 0, 0);
+}
 
 extern "C" int Main(void *parent) {
 	PutStr("Hello, from userland!\r\n");
 
-	uintptr_t cnodePtr = 0;
-	size_t cnodeSlot = 4;
+	uptr cnodeCapPtr = 0;
+	usize cnodeSlot = 4;
 
-	MKMI_Capability cnodeCapability;
 	MKMI_Capability memoryCapability;
 
-	Syscall(2, 2, cnodePtr, cnodeSlot, (size_t)&cnodeCapability, 0, 0);
+	Syscall(SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_GET_PTR, 0, cnodeSlot, (usize)&cnodeCapPtr, 0, 0);
 	cnodeSlot = 1;
 
 	PutStr("Memory regions:\r\n");
 	do {
-		Syscall(2, 2, cnodeCapability.Object, cnodeSlot, (size_t)&memoryCapability, 0, 0);
+		Syscall(SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_GET_CAP, cnodeCapPtr, cnodeSlot, (usize)&memoryCapability, 0, 0);
 		
 		PutHex(memoryCapability.Type);
 		PutStr("\r\n");
@@ -37,6 +39,53 @@ extern "C" int Main(void *parent) {
 
 		++cnodeSlot;
 	} while(memoryCapability.Type != MKMI_ObjectType::NULL_CAPABILITY);
+		
+	Syscall(SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_GET_CAP, cnodeCapPtr, 1, (usize)&memoryCapability, 0, 0);
+
+	int result = 0;
+	uptr addr = memoryCapability.Object;
+	uptr virt = 0x40000000;
+
+	PutStr("Mapping ");
+	PutHex(addr);
+	PutStr("...\r\n");
+
+	Syscall(SYSCALL_VECTOR_ARCHCTL, SYSCALL_ARCHCTL_MAP_INTERMEDIATE, (usize)&result, 3, addr, virt, PAGE_PROTECTION_READ | PAGE_PROTECTION_WRITE);
+	PutDec(result);
+	PutStr("\r\n");
+	if (result == 1) {
+		result = 0;
+	} else {
+		addr += 0x1000;
+	}
+
+	Syscall(SYSCALL_VECTOR_ARCHCTL, SYSCALL_ARCHCTL_MAP_INTERMEDIATE, (usize)&result, 2, addr, virt, PAGE_PROTECTION_READ | PAGE_PROTECTION_WRITE);
+	PutDec(result);
+	PutStr("\r\n");
+	if (result == 1) {
+		result = 0;
+	} else {
+		addr += 0x1000;
+	}
+
+	Syscall(SYSCALL_VECTOR_ARCHCTL, SYSCALL_ARCHCTL_MAP_INTERMEDIATE, (usize)&result, 1, addr, virt, PAGE_PROTECTION_READ | PAGE_PROTECTION_WRITE);
+	PutDec(result);
+	PutStr("\r\n");
+	if (result == 1) {
+		result = 0;
+	} else {
+		addr += 0x1000;
+	}
+
+	Syscall(SYSCALL_VECTOR_ARCHCTL, SYSCALL_ARCHCTL_MAP_PAGE, (usize)&result, addr, virt, PAGE_PROTECTION_READ | PAGE_PROTECTION_WRITE, 0);
+	PutDec(result);
+	PutStr("\r\n");
+
+	*(u32*)virt = 0x69;
+	PutHex(virt);
+	PutStr(" -> ");
+	PutHex(*(u32*)virt);
+	PutStr("\r\n");
 
 	(void)parent;
 	return 0;
