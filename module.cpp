7653c1001f93bd2c625bf6a279623882895a11ca
Filippo Mutta <filippo.mutta@gmail.com>
2024-07-20 19:28:12 +0200

    Mock heap.

--- module.cpp
+++ module.cpp
@@ -8,6 +8,18 @@ struct ut_header {
 	u32 flags;
 }__attribute__((packed));
 
+struct heap_header {
+	heap_header *previous, *next;
+	usize size;
+	bool used;
+}__attribute__((packed));
+
+heap_header *firstHeader;
+
+void *malloc(usize size);
+void free(void *ptr);
+void dump_hdrs();
+
 usize get_capability_pointer(uptr node, usize slot) {
 	uptr pointer = 0;
 	syscall(5, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_GET_PTR, node, slot, (usize)&pointer);
@@ -80,9 +92,14 @@ extern "C" int Main(int argc, char **argv) {
 	usize lvl3_slot;
 	usize lvl2_slot;
 	usize lvl1_slot;
-	const usize frame_count = 10;
+	const usize frame_count = 512;
 	usize frame_slot[frame_count];
+	ut_header new_hdr;
+
+	get_ut_header(cap_ptr, new_ut_slot + 1, &new_hdr);
+	mkmi_log("Region size: %d frames\r\n", new_hdr.length / 4096);
 	syscall(8, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_SPLIT, cap_ptr, new_ut_slot + 1, 4096, new_cap_ptr, (usize)&ut_frame_slot, 3 + frame_count + 1);
+
 	syscall(8, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_RETYPE, new_cap_ptr, ut_frame_slot, OBJECT_TYPE::PAGING_STRUCTURE, new_cap_ptr, (usize)&lvl3_slot, CAPABILITY_RIGHTS::ACCESS, 0);
 	syscall(8, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_RETYPE, new_cap_ptr, ut_frame_slot + 1, OBJECT_TYPE::PAGING_STRUCTURE, new_cap_ptr, (usize)&lvl2_slot, CAPABILITY_RIGHTS::ACCESS, 0);
 	syscall(8, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_RETYPE, new_cap_ptr, ut_frame_slot + 2, OBJECT_TYPE::PAGING_STRUCTURE, new_cap_ptr, (usize)&lvl1_slot, CAPABILITY_RIGHTS::ACCESS, 0);
@@ -91,7 +108,7 @@ extern "C" int Main(int argc, char **argv) {
 		syscall(8, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_RETYPE, new_cap_ptr, ut_frame_slot + 3 + i, OBJECT_TYPE::FRAMES, new_cap_ptr, (usize)&frame_slot[i], CAPABILITY_RIGHTS::ACCESS, 0);
 	}
 
-	uptr addr = 0xDEAD0000;
+	uptr addr = 0xDEAD000000;
 	syscall(8, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_MAP_INTERMEDIATE, new_cap_ptr, lvl3_slot, 3, addr, PAGE_PROTECTION_READ | PAGE_PROTECTION_WRITE, 0, 0);
 	syscall(8, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_MAP_INTERMEDIATE, new_cap_ptr, lvl2_slot, 2, addr, PAGE_PROTECTION_READ | PAGE_PROTECTION_WRITE, 0, 0);
 	syscall(8, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_MAP_INTERMEDIATE, new_cap_ptr, lvl1_slot, 1, addr, PAGE_PROTECTION_READ | PAGE_PROTECTION_WRITE, 0, 0);
@@ -100,11 +117,154 @@ extern "C" int Main(int argc, char **argv) {
 		syscall(8, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_MAP_PAGE, new_cap_ptr, frame_slot[i], addr + 4096 * i, PAGE_PROTECTION_READ | PAGE_PROTECTION_WRITE, 0, 0);
 	}
 
-	*((u32*)addr) = 0x0BADCACA;
-	mkmi_log("Value: 0x%x\r\n", *((u32*)addr));
+	for (usize i = 0; i < frame_count * 4096; ++i) {
+		*((volatile u8*)(addr + i)) = 0x00;
+	}
 
-/*
+	heap_header *hdr = (heap_header*)addr;
+	hdr->previous = hdr->next = NULL;
+	hdr->used = false;
+	hdr->size = frame_count * 4096 - sizeof(heap_header);
+	firstHeader = hdr;
+	
+	mkmi_log("Heap at 0x%x of size %d bytes\r\n", addr, frame_count * 4096);
+
+	void *memareas[8];
+	dump_hdrs();
+	memareas[0] = malloc(128);
+	dump_hdrs();
+	memareas[1] = malloc(256);
+	dump_hdrs();
+	memareas[2] = malloc(512);
+	dump_hdrs();
+	memareas[3] = malloc(1024);
+	dump_hdrs();
+	memareas[4] = malloc(2048);
+	dump_hdrs();
+	memareas[5] = malloc(4096);
+	dump_hdrs();
+	memareas[6] = malloc(8192);
+	dump_hdrs();
+	memareas[7] = malloc(16384);
+	dump_hdrs();
+
+
+
+	free(memareas[0]);
+	dump_hdrs();
+	free(memareas[2]);
+	dump_hdrs();
+	free(memareas[4]);
+	dump_hdrs();
+	free(memareas[6]);
+	dump_hdrs();
+	free(memareas[1]);
+	dump_hdrs();
+	free(memareas[3]);
+	dump_hdrs();
+	free(memareas[5]);
+	dump_hdrs();
+	free(memareas[7]);
+	dump_hdrs();
+
+	while(true) { }
+
+	syscall(3, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_DEBUG, 0);
+	syscall(3, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_DEBUG, cap_ptr);
 	syscall(3, SYSCALL_VECTOR_CAPCTL, SYSCALL_CAPCTL_DEBUG, new_cap_ptr);
-*/
+	
 	return 0;
+}
+
+void dump_hdrs() {
+	mkmi_log("HEAP_START\r\n");
+	for (heap_header *hdr = firstHeader; hdr != NULL; hdr = hdr->next) {
+		if (!hdr) {
+			break;
+		}
+
+		mkmi_log("HEAP_HDR 0x%x, %d bytes, %s\r\n", (void*)hdr, hdr->size, hdr->used ? "used" : "unused");
+	}
+	mkmi_log("HEAP_END\r\n");
+}
+
+void *malloc(usize size) {
+	for (heap_header *hdr = firstHeader; hdr != NULL; hdr = hdr->next) {
+		if (!hdr) break;
+		if (hdr->used) continue;
+		if (hdr->size < size) continue;
+		
+		if (hdr->size == size) {
+			hdr->used = true;
+			return (void*)((uptr)hdr + sizeof(heap_header));
+		} else if (hdr->size > size) {
+			if (hdr->size > size + sizeof(heap_header)) {
+				heap_header *next = (heap_header*)((uptr)hdr + sizeof(heap_header) + size);
+				next->used = false;
+				next->size = hdr->size - size - sizeof(heap_header);
+				hdr->size -= next->size + sizeof(heap_header);
+
+				next->previous = hdr;
+				next->next = hdr->next;
+
+				if (hdr->next) {
+				    hdr->next->previous = next;
+				}
+
+				hdr->next = next;
+
+				hdr->used = true;
+				return (void*)((uptr)hdr + sizeof(heap_header));
+			} else {
+				hdr->used = true;
+				return (void*)((uptr)hdr + sizeof(heap_header));
+			}
+		} else {
+			continue;
+		}
+	}
+
+	return NULL;
+}
+
+void free(void *ptr) {
+	heap_header *hdr = (heap_header*)((uptr)ptr - sizeof(heap_header));
+
+	hdr->used = false;
+
+	bool cleanup = false;
+	while (true) {
+		if (hdr->next && !hdr->next->used) {
+			cleanup = true;
+
+			heap_header *nextHdr = hdr->next;
+			if (nextHdr->next) {
+				nextHdr->previous = hdr;
+			}
+
+			hdr->size += nextHdr->size + sizeof(heap_header);
+			hdr->next = nextHdr->next;
+		}
+
+		if (hdr->previous && !hdr->previous->used) {
+			cleanup = true;
+
+			heap_header *prevHdr = hdr->previous;
+
+			if(hdr->next) {
+				hdr->next->previous = prevHdr;
+			}
+
+			prevHdr->next = hdr->next;
+			prevHdr->size += hdr->size + sizeof(heap_header);
+
+			hdr = prevHdr;
+		}
+
+		if (cleanup) {
+			cleanup = false;
+		} else {
+			break;
+		}
+	}
 }
