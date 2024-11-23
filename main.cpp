5ef77c5e898b7578ccf68cbf22978f708e91b1f5
Filippo Mutta <filippo.mutta@luxonis.com>
2024-11-23 15:16:19 +0100

    CPUID test.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cpuid.h>
 #include <stdint.h>
 #include <stddef.h>
 #include <mkmi.h>
@@ -17,10 +18,12 @@ extern "C" int Main(int argc, char **argv) {
 	mkmi_log("Hello from the containerized process.\r\n");
 	uptr capability = 0;
 	VMMCall(0xDEAD);
-	VMMCall(0xB337);
-	VMMCall(0x10EF);
-	VMMCall(0x7071);
 	VMMCall(0x8086);
+
+	u32 vendor[4] = {};
+	u32 null = 0;
+	__cpuid(0x40000000, null, vendor[0], vendor[1], vendor[2]);
+	mkmi_log("Vendor hypervisor: 0x%x, %s\r\n", null, vendor);
 
 	uptr  obj= 0;
 	__fast_syscall(SYSCALL_VECTOR_SEARCH_CAPABILITY, (uptr)0, UNTYPED_FRAMES, (uptr)&capability, 0, 0, 0);
