4c0979d9b72388b9eab5a0b349c7f6cb99566be5
Filippo Mutta <filippo.mutta@luxonis.com>
2024-12-03 07:59:10 +0100

    Moved VMM to new file.

--- memory.cpp
+++ memory.cpp
@@ -4,10 +4,6 @@
 #include "capability.hpp"
 
 
-static void MapIntermediate(Capability capability, usize level, uptr addr, usize flags);
-static void MapPage(Capability frame, uptr addr, usize flags);
-
-static VirtualMemoryHeader Headers[MAX_MEMORY_HEADERS];
 static Capability *MemoryMap;
 static bool *Usable;
 static usize RegionCount;
@@ -45,61 +41,3 @@ int GetUntypedRegion(usize size, Capability *capability) {
 	return GUNTPD_NOMEM;
 }
 
-int MMapPage(Capability cap, uptr addr, usize flags) {
-	VirtualMemoryHeader *header = &Headers[addr / MAX_LVL3_LENGTH];
-
-	/*
-	if (header->LVL3Start > addr || header->LVL3Start + header->LVL3Length < addr + cap.Size || header->LVL3Length == 0) return MMAP_LVL3;
-	if (header->LVL2Start > addr || header->LVL2Start + header->LVL2Length < addr + cap.Size || header->LVL2Length == 0) return MMAP_LVL2;
-	if (header->LVL1Start > addr || header->LVL1Start + header->LVL1Length < addr + cap.Size || header->LVL1Length == 0) return MMAP_LVL1;*/
-
-	MapPage(cap, addr, flags);
-
-	return MMAP_OK;
-
-}
-
-int MMapIntermediate(Capability cap, usize level, uptr addr, usize flags) {
-	VirtualMemoryHeader *header = &Headers[addr / MAX_LVL3_LENGTH];
-
-	if (level == 3) {
-		MapIntermediate(cap, level, addr, flags);
-		header->LVL3Start = (addr >> 39) << 39;
-		header->LVL3Length = MAX_LVL3_LENGTH;
-
-		return MMAP_OK;
-	}
-
-	//if (header->LVL3Length == 0) return -MMAP_LVL3;
-
-	if (level == 2) {
-		MapIntermediate(cap, level, addr, flags);
-		header->LVL2Start = (addr >> 30) << 30;
-		header->LVL2Length = MAX_LVL2_LENGTH;
-
-		return MMAP_OK;
-	}
-
-	//if (header->LVL2Start < addr || header->LVL2Start + header->LVL2Length < addr + cap.Size || header->LVL2Length == 0) return -MMAP_LVL2;
-
-	if (level == 1) {
-		MapIntermediate(cap, level, addr, flags);
-		header->LVL1Start = (addr >> 21) << 21;
-		header->LVL1Length = MAX_LVL1_LENGTH;
-
-		return MMAP_OK;
-	}
-
-	//if (header->LVL1Start < addr || header->LVL1Start + header->LVL1Length < addr + cap.Size || header->LVL1Length == 0) return -MMAP_LVL1;
-
-	return MMAP_ERR;
-
-}
-
-void MapIntermediate(Capability capability, usize level, uptr addr, usize flags) {
-	__fast_syscall(SYSCALL_VECTOR_MAP_INTERMEDIATE_CAPABILITY, capability.Object, level, addr, flags , 0, 0);
-}
-
-void MapPage(Capability frame, uptr addr, usize flags) {
-	__fast_syscall(SYSCALL_VECTOR_MAP_CAPABILITY, frame.Object, FRAME_MEMORY, addr, flags , 0, 0);
-}
