58cdad99b5dce850ff0afeb1832ec254e73bfbe4
Filippo Mutta <filippo.mutta@luxonis.com>
2024-12-26 17:16:37 +0100

    Added ARP header.

--- capability.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <object.hpp>
-
-void AddressCapability(uptr object, Capability *cap);
-void SplitCapability(Capability capability, Capability *capabilities, usize splitCount, usize splitSize);
-void RetypeCapability(Capability capability, Capability *result, OBJECT_TYPE kind, usize count);
-
--- serial.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-int InitSerial();
-char ReadSerial();
-void ReadSerial(char *s, int c);
-void WriteSerial(char a);
-void WriteSerial(char *s);
